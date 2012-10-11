a3705910422336827a557e63e2d5d8e0c576f216
Yuvi Masory <ymasory@gmail.com>
2012-10-11 16:28:55 -0400

    temporarily redirect due to dns issues

--- src/main/scala/com/scalatypes/site/RootServlet.scala
+++ src/main/scala/com/scalatypes/site/RootServlet.scala
@@ -6,12 +6,7 @@ import scalate.ScalateSupport
 class RootServlet extends ScalatraServlet with ScalateSupport {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    redirect("http://scalatypes.libsyn.com")
   }
 
   notFound {
